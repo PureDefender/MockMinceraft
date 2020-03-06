172bd2cd44612c8c1cc4b781cf4c74ec8b55a933
Luke Doukakis <35055929+lukedoukakis@users.noreply.github.com>
2020-03-05 16:25:21 -0800

    Added FirstPersonCameraController class to control camera POV, and Vector3Float class to represent camera's coordinates in 3D space

---
+++ src/fakeminceraft/Vector3Float.java
@@ -0,0 +1,19 @@
+
+package fakeminceraft;
+
+/**
+ *
+ * @author Luke
+ */
+
+// class to represent camera's position in 3D space using float values
+public class Vector3Float {
+    
+    public float x, y, z;
+    
+    public Vector3Float(int x, int y, int z){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+}
